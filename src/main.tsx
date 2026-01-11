3de5a50c8d9363a3874cd579df5a2d4822c8e118
somilkwatra <somilkwatra21@gmail.com>
2026-01-11 15:20:35 +0530

    feat: remove TodoList route from main application

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
-import TodoList from "./components/TodoList/TodoList.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/todolist" element={<TodoList />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
