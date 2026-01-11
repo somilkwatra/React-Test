d1058e11520354137e585b03ba5e832da9f1ef0d
somilkwatra <somilkwatra21@gmail.com>
2026-01-11 14:50:01 +0530

    feat: add TodoList component and routing; update styles for button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import TreeView from "./components/TreeNode/TreeView";
 import { initialTreeData } from "./data/treeData";
 import "./styles/global.css";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
+
   return (
     <div className="app-container">
       <h2>Tree View Component</h2>
       <TreeView data={initialTreeData} />
+
+      <button className="btn-blue" onClick={() => navigate("/todolist")}>
+        Go to Todo List
+      </button>
     </div>
   );
 }
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,10 @@
+
+function TodoList() {
+  return (
+    <div>
+      <h2>Todo List Component</h2>
+    </div>
+  );
+}
+
+export default TodoList;
